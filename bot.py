569fd6b9605001afcfde394cd8322da104f88d75
notHaze <combet.florent@gmail.com>
2022-03-12 12:02:32 +0100

    lyrics add

--- bot.py
+++ bot.py
@@ -343,7 +343,7 @@ class Music(commands.Cog):
 
         if not vc or not vc.is_playing():
             return await ctx.send('I am not currently playing anything!')
-        print(vc.source.web_url)
+        print(vc.source.web_url.strip("https://www.youtube.com/watch?v="))
         video_id=vc.source.web_url
         """.strip("https://www.youtube.com/watch?v=")"""
         lyrics = YouTubeTranscriptApi.get_transcripts(video_id, languages=['fr', 'en'])
